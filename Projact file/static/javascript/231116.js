5f8e89514bac6d2db3f55e7ff69222be0c84c13b
Kimminsu012 <asdfg002269@gmail.com>
2023-11-17 09:30:44 +0900

    231116 과제

--- Projact file/static/javascript/231116.js
+++ Projact file/static/javascript/231116.js
@@ -16,26 +16,50 @@
 
 
 var min = parseInt(prompt("주차한 시간을 입력 하시오 (분단위)"));
-
 var money = 1000
 
-if( min <= 30 ){
-    document.write(money) ;
-}else if( ((min > 30)/10) && (min < 120) ){
-    min = parseInt(min-30);
-    money = parseInt(min / 10) * 100 + money;
-    document.write(money) ;
-    }else if( ( min >= 120 ) && ( min < 240 )){
-        min = parseInt(min-120);
-        money = 1500;
-        money = parseInt(min / 10) * 100 + money;
-        document.write(money) ;
-    }else if( (min >= 240) && (min < 480)){
-        min = parseInt(min-240);
-        money = 2500
-        money = parseInt(min / 10) * 100 + money;
-        document.write(money);
-    }else if( min >= 480){
-        money = 10000
-        document.write(money);
+if( min >= 480 ){
+    money=10000; min=0;
+}else if( min >= 240 ){
+    money=2500; min = min-240;
+}else if( min >= 120 ){
+    money=1500; min = min-120;
+}else{
+    min = min < 30 ? 0 : min-30;
 }
+
+var price = Math.floor(min/10)*100 + money;
+document.write("주차요금 : " + price + "원");
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if( min <= 30 ){
+//     document.write(money) ;
+// }else if((min < 120) ){
+//     min = parseInt(min-30);
+//     money = parseInt(min / 10) * 100 + money;
+//     document.write(money) ;
+//     }else if( ( min < 240 ) ){
+//         min = parseInt(min-120);
+//         money = 1500;
+//         money = parseInt(min / 10) * 100 + money;
+//         document.write(money) ;
+//     }else if((min < 480) ){
+//         min = parseInt(min-240);
+//         money = 2500
+//         money = parseInt(min / 10) * 100 + money;
+//         document.write(money);
+//     }else if( min >= 480 ){
+//         money = 10000
+//         document.write(money);
+// }
