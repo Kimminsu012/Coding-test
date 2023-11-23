bba203e2e63788545f6c2ae79b91995b0f1e744a
Kimminsu012 <asdfg002269@gmail.com>
2023-11-23 09:35:07 +0900

    231122 과제

--- Projact file/static/javascript/231122.js
+++ Projact file/static/javascript/231122.js
@@ -11,10 +11,36 @@
 const names = ["주현상","윤대경","이민우","이태양","페냐","박상원","장시환"];
 const era = [1.96 , 2.45 , 2.63 , 3.23 , 3.60 , 3.65 , 3.38];
 let idx = new Array();
+let t_idx = new Array();
 let total_up = new Array();
 let avg = 0;
 let total = 0;
+let nt = new Array();
 
+
+
+// for(var i=0; i<names.length; i++){
+//     idx.push(names[i]);
+// }document.write(idx + "<br>");
+
+// for(var i=0; i<era.length; i++){
+//     t_idx.push(era[i]);
+// }document.write(t_idx + "<br>");
+
+// for(var i=0; i<era.length; i++){
+//     total += era[i];
+//     avg = total/era.length;
+// }document.write("평균 : " + avg + "<br>");
+
+// for(var i=0; i<names.length; i++){
+//     nt[i] = names[i] + era[i];
+// }document.write(nt + "<br>");
+
+// for(var i=0; i<names.length; i++){
+//     if(avg<era[i]){
+//         total_up.push(names[i]);
+//     }document.write(total_up);
+// }
 
 
 
@@ -25,9 +51,29 @@ for(var i=0; i<era.length; i++){
 
 
 for(var i=0; i<names.length; i++){
-    if(era[i]<avg){
+    if(era[i]>=avg){
         idx.push(names[i]);
         total_up.push(era[i]);
     }
 }
 document.write("이름 : " + idx + "<br>" + "실책점 : " + total_up);
+
+// const names = ["주현상","윤대경","이민우","이태양","페냐","박상원","장시환"];
+// const era = [1.96 , 2.45 , 2.63 , 3.23 , 3.60 , 3.65 , 3.38];
+// let idx = new Array();
+// let total = 0; // 평균자책점(era)의 총점을 구하기 위한 누적 변수
+
+
+
+// for(var i=0; i<era.length; i++){
+//     total += era[i]; // total = total + era[i] // 평균자책점(era) 총점구하기
+    
+// }
+
+// let avg = total/era.length; // 평균 자책점(era) 평균 구하기
+
+// for(var i=0; i<names.length; i++){
+//     if(era[i]>=avg){ // era의 평균이상인 값 찾기
+//         document.write( names[i] + " "); // era 평균이상인 값의 인덱스를 name의 인덱스로사용
+//     }
+// }
