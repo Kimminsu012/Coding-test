931e676cebb7ded6432d3d0e8c2f4a3915a24abd
Kimminsu012 <asdfg002269@gmail.com>
2023-11-23 09:05:11 +0900

    231122 과제

--- Projact file/static/javascript/231122.js
+++ Projact file/static/javascript/231122.js
@@ -10,35 +10,24 @@
 
 const names = ["주현상","윤대경","이민우","이태양","페냐","박상원","장시환"];
 const era = [1.96 , 2.45 , 2.63 , 3.23 , 3.60 , 3.65 , 3.38];
-let n_idx = new Array();
-let t_idx = new Array();
+let idx = new Array();
 let total_up = new Array();
 let avg = 0;
 let total = 0;
-let nt = new Array();
 
 
 
-for(var i=0; i<names.length; i++){
-    n_idx.push(names[i]);
-}document.write(n_idx + "<br>");
-
-for(var i=0; i<era.length; i++){
-    t_idx.push(era[i]);
-}document.write(t_idx + "<br>");
 
 for(var i=0; i<era.length; i++){
     total += era[i];
     avg = total/era.length;
 }document.write("평균 : " + avg + "<br>");
 
-for(var i=0; i<names.length; i++){
-    nt[i] = names[i] + era[i];
-}document.write(nt + "<br>");
 
 for(var i=0; i<names.length; i++){
-    if(avg<era[i]){
-        total_up.push(names[i]);
-    }document.write(total_up);
+    if(era[i]<avg){
+        idx.push(names[i]);
+        total_up.push(era[i]);
+    }
 }
-
+document.write("이름 : " + idx + "<br>" + "실책점 : " + total_up);
