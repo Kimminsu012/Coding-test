5d249e1fc8bf649d4fd5aee045366a3835b4b39d
Kimminsu012 <asdfg002269@gmail.com>
2023-11-28 11:13:42 +0900

    빙고게임

--- 231123/static/javascript/game.js
+++ 231123/static/javascript/game.js
@@ -14,6 +14,11 @@ for( var i=0; i<=25; i++){
 
 $(function(){
 
+    // 선생님 풀이
+    // $(".modal_bg").on("click",function(){
+    //     $("#result_modal").hide();
+    // });
+
     $.each( board , function(i,v){ // i-인덱스 , v-배열값
         $(".numBox").eq(i).text(v);
     });
@@ -31,6 +36,34 @@ $(function(){
 
 // 열 +1 , 행 +5 , 대각 +6 규칙
 function endgame(){
+
+    // 선생님 풀이
+    // var row=0, col=0, end=0, cross=[0,0];
+    // for( var i=0; i<5; i++ ){ // 빙고 확인하기
+    //     for( var k=0; k<5; k++ ){
+    //         if( board[i*5+k] == 0) row++; // 가로방향으로 5줄
+    //         if( board[k*5+i] == 0) col++; // 세로방향으로 5줄
+    //     }
+    //     if( board[i*6] == 0) cross[0]++; // 왼->오 대각선
+    //     if( board[(i+1)*4] == 0) cross[1]++; // 오->왼 대각선
+    //     if(cross[1]==5) end++;
+    //     if(cross[0]==5) end++;
+    //     if(row == 5) end++;
+    //     if(col == 5) end++;
+
+    //     row=0; // 한줄씩 확인 할 때 마다 0개 부터 확인해야하므로 초기화
+    //     col=0; // 1열씩 확인 할 때 마다 0개 부터 확인해야하므로 초기화
+    // }
+
+    // if( end == 5){ // 5줄 빙고 완성
+    //     $("#result_modal").show(); // $("#result_modal").css("display","block");
+    //     $(".result").text("5줄 빙고 완성");
+    // }else if( end >= 6){ // 빙고 실패
+    //     $("#result_modal").show();
+    //     $(".result").text("6줄이상 빙고 실패");
+    // }
+
+
     let result = 0;
 
     if(sc.length >= 1){
