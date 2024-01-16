4d9ebafcd99f4396bca9c8cb3dfcb72e4b9c9fd3
kimminsu012 <asdfg002269@gmail.com>
2024-01-16 17:28:57 +0900

    프로젝트

--- 프로젝트/static/javascript/page.js
+++ 프로젝트/static/javascript/page.js
@@ -3,25 +3,33 @@
 
 
 
-document.onload = function(){
+$(function(){
 
-// 롤링 배너 복제본 생성
-let roller = document.querySelector('#top_img_box');
-roller.id = 'roller1'; // 아이디 부여
-
-let clone = roller.cloneNode(true)
-// cloneNode : 노드 복제. 기본값은 false. 자식 노드까지 복제를 원하면 true 사용
-clone.id = 'roller2';
-document.querySelector('.main_img_box').appendChild(clone); // wrap 하위 자식으로 부착
-
-document.querySelector('#roller1').style.left = '0px';
-document.querySelector('#roller2').style.left = document.querySelector('.top_ul').offsetWidth + 'px';
-// offsetWidth : 요소의 크기 확인(margin을 제외한 padding값, border값까지 계산한 값)
-
-roller.classList.add('original');
-clone.classList.add('clone');
-        
+    let pu = 0;
     
+    
+
+    $("#slide_bt").on("click",function(){
+        
+        if(pu == 0){
+        $("#top_img_box").css("animation-play-state","paused");
+        $("#bottom_img_box").css("animation-play-state","paused");
+        pu++;
+        }
+        
+
+    })
+    
+    $("#slide_bt").on("click",function(){
+        if(pu == 1){
+        $("#top_img_box").css("animation-play-state","running");
+        $("#bottom_img_box").css("animation-play-state","running");
+        pu--;
     }
 
+    })
+    
+    
 
+
+});
